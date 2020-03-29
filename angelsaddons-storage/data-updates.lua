e297f378292c4d6cbb69740f2099471ada3e5da2
Tim Vroom <800147+TimVroom@users.noreply.github.com>
2020-03-29 17:58:31 +0200

    More proper ordering for the category group when in industries

--- angelsaddons-storage/data-updates.lua
+++ angelsaddons-storage/data-updates.lua
@@ -18,22 +18,8 @@ end
 
 if angelsmods.industries then
   if angelsmods.addons.oresilos then
-    data:extend(
-      {
-        {
-          type = "item-subgroup",
-          name = "angels-silo",
-          group = "angels-logistics",
-          order = "ac[chests-silo]"
-        }
-      }
-    )
-    data.raw["item"]["silo"].subgroup = "angels-silo"
-    data.raw["item"]["silo-active-provider"].subgroup = "angels-silo"
-    data.raw["item"]["silo-passive-provider"].subgroup = "angels-silo"
-    data.raw["item"]["silo-requester"].subgroup = "angels-silo"
-    data.raw["item"]["silo-storage"].subgroup = "angels-silo"
-    data.raw["item"]["silo-buffer"].subgroup = "angels-silo"
+    data.raw["item-subgroup"]["angels-silos"].group = "angels-logistics"
+    data.raw["item-subgroup"]["angels-silos"].order = "ac[chests-silo]"
   end
 
   if angelsmods.addons.warehouses then
@@ -46,6 +32,7 @@ end
 -- PETROCHEM
 if angelsmods.petrochem then
   data.raw["item"]["angels-pressure-tank-1"].subgroup = "angels-fluid-tanks"
+  data.raw["item"]["angels-pressure-tank-1"].order = "e-" .. data.raw["item"]["angels-pressure-tank-1"].order
   data.raw["technology"]["pressure-tanks"].prerequisites = {"angels-fluid-control"}
 
   if bobmods and bobmods.logistics then
